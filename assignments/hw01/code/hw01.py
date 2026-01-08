c4f94862250288cd8fa63ed5e7f235c95ed496d4
PALMER SWANSON <pjswanson@fsu.edu>
2026-01-08 13:52:46 -0500

    working on part 2

--- assignments/hw01/code/hw01.py
+++ assignments/hw01/code/hw01.py
@@ -4,8 +4,8 @@ from sklearn import tree
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
+#problem 1
 #loading in the data
-
 #file was seperated via spaces.  We know that its dimensions are 6000 rows and 5000 columns.  
 #needed to use delim_whitespace instead of sep, and specify no header
 train_data = pd.read_csv("assignments/hw01/data/gisette_train.data", delim_whitespace=True, header=None)
@@ -45,12 +45,36 @@ for i in range(1, 13):
 plt.figure()
 plt.plot(range(1,13), training_missclass, marker='o', label='Training error')
 plt.plot(range(1,13), validate_missclass, marker='o', label='Validation error')
-plt.title("Training and Test Misclassification Rates")
-plt.subtitle("Gisette Dataset")
+plt.title("Training and Test Misclassification Rates, Gisette Dataset")
 plt.xlabel("Maximum depth")
 plt.ylabel("Misclassification error")
 plt.legend()
 plt.grid(True, alpha=0.3)
-plt.show()
+#plt.show()
 
 #save this image into the figures folder
+plt.savefig("assignments/hw01/figures/gisette_misclassification_p1.png", dpi=400, bbox_inches="tight")
+plt.close()
+
+#need to create a dataframe for table:
+results_q1 = pd.DataFrame({
+    "Depth": range(1,13),
+    "Training Misclassification": training_missclass,
+    "Validation Misclassification": validate_missclass
+})
+
+#find lowest validation error and tree depth
+lowest_error = results_q1[results_q1['Validation Misclassification'] == results_q1['Validation Misclassification'].min()]
+
+#save for latex.  Discovered you can use the .to_latex command. 
+LATEX_table = lowest_error.to_latex(
+    index=False,
+    caption="Lowest misclassification error",
+    label="tab:tree_misclass_error",
+    column_format="ccc"
+)
+
+print(LATEX_table)
+
+#problem 2, need to repeat this for satimage, madelon, hill-valley, and covtype data
+datasets = ["satimage", "madelon", "hill-valley", "covtype"]
