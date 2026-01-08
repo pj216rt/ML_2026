e2650998fc368cae94237c5d50052a233fd4653b
PALMER SWANSON <pjswanson@fsu.edu>
2026-01-08 10:25:58 -0500

    working on hw1

--- assignments/hw01/code/hw01.py
+++ assignments/hw01/code/hw01.py
@@ -1,3 +1,11 @@
 import pandas as pd
 
 #loading in the data
+
+file_path = "assignments/hw01/data/gisette_train.data"
+
+#file was seperated via spaces.  We know that its dimensions are 6000 rows and 5000 columns.  
+#needed to use delim_whitespace instead of sep, and specify no header
+data = pd.read_csv(file_path, delim_whitespace=True, header=None)
+
+print(data.shape)
