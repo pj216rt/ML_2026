b9c78623d0d54942b9c75ac13c7fbe758b41545a
PALMER SWANSON <pjswanson@fsu.edu>
2026-01-09 17:29:30 -0500

    clearner implimentation

--- assignments/hw01/code/hw01.py
+++ assignments/hw01/code/hw01.py
@@ -5,309 +5,121 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 
-#problem 1
-#loading in the data
-#file was seperated via spaces.  We know that its dimensions are 6000 rows and 5000 columns.  
-#needed to use delim_whitespace instead of sep, and specify no header
-train_data = pd.read_csv("assignments/hw01/data/gisette_train.data", delim_whitespace=True, header=None)
-train_labels = pd.read_csv("assignments/hw01/data/gisette_train.labels", header=None)
+#need a double loop.  Outer loop over datasets, inner loop over depts
 
-validate_data = pd.read_csv("assignments/hw01/data/gisette_valid.data", delim_whitespace=True, header=None)
-validate_labels = pd.read_csv("assignments/hw01/data/gisette_valid.labels", header=None)
+depths = range(1, 13)
 
-#now we can use it to make predictions on the train and test (really validate)
+#creating a dict to help loop over the datasets more
+datasets = [
+    {
+        "name": "Gisette",
+        "X_train": "assignments/hw01/data/gisette_train.data",
+        "y_train": "assignments/hw01/data/gisette_train.labels",
+        "X_valid": "assignments/hw01/data/gisette_valid.data",
+        "y_valid": "assignments/hw01/data/gisette_valid.labels",
+        "fig_out": "assignments/hw01/figures/gisette_misclassification_error.png",
+        "latex_caption": "Lowest misclassification error, Gisette Dataset",
+        "latex_label": "tab:gissette_tree_misclass_error"
+    },
+    {
+        "name": "Satimage",
+        "X_train": "assignments/hw01/data/X_sat.dat",
+        "y_train": "assignments/hw01/data/Y_sat.dat",
+        "X_valid": "assignments/hw01/data/Xtest_sat.dat",
+        "y_valid": "assignments/hw01/data/Ytest_sat.dat",
+        "fig_out": "assignments/hw01/figures/satimage_misclassification_error.png",
+        "latex_caption": "Lowest misclassification error: Satimage Dataset",
+        "latex_label": "tab:Satimage_tree_misclass_error"
+    },
+    {
+        "name": "Madelon",
+        "X_train": "assignments/hw01/data/madelon_train.data",
+        "y_train": "assignments/hw01/data/madelon_train.labels",
+        "X_valid": "assignments/hw01/data/madelon_valid.data",
+        "y_valid": "assignments/hw01/data/madelon_valid.labels",
+         "fig_out": "assignments/hw01/figures/madelon_misclassification_error.png",
+        "latex_caption": "Lowest misclassification error: Madelon Dataset",
+        "latex_label": "tab:Madelon_tree_misclass_error"
+    },
+    {
+        "name": "Hill-Valley",
+        "X_train": "assignments/hw01/data/X_hv.dat",
+        "y_train": "assignments/hw01/data/Y_hv.dat",
+        "X_valid": "assignments/hw01/data/Xtest_hv.dat",
+        "y_valid": "assignments/hw01/data/Ytest_hv.dat",
+        "fig_out": "assignments/hw01/figures/hill_valley_misclassification_error.png",
+        "latex_caption": "Lowest misclassification error: Hill-Valley Dataset",
+        "latex_label": "tab:hill_valley_tree_misclass_error"
+    },
+    {
+        "name": "Covtype",
+        "X_train": "assignments/hw01/data/covtype.data",
+        
+    }
+]
 
-#need to loop from 1 to 12
-#initialize the empty storage arrays
-training_missclass = []
-validate_missclass = []
 
-#12 depths
-for i in range(1, 13):
-    #fitting the decision treee with max depth of i
-    dtree = tree.DecisionTreeClassifier(max_depth=i)
-    dtree = dtree.fit(train_data,train_labels)
+for i in datasets:
+    #reading in dataset, if dataset is not covtype
+    X_train = pd.read_csv(i["X_train"], delim_whitespace=True, header=None)
+    X_valid = pd.read_csv(i["X_valid"], delim_whitespace=True, header=None)
 
-    #predict
-    training_predict = dtree.predict(train_data)
-    validate_predict = dtree.predict(validate_data)
+    y_train = pd.read_csv(i["y_train"], header=None)
+    y_valid = pd.read_csv(i["y_valid"], header=None)
 
-    #get misclassification rate
-    #check accuracy scores.  Returns fraction of correctly classified sampler.  I think that this means that the
-    # #misclassification rate is simply 1- this value
-    training_missclass_rate = 1 - accuracy_score(train_labels, training_predict)
-    validation_missclass_rate = 1 - accuracy_score(validate_labels, validate_predict)
 
-    #append to arrays
-    training_missclass.append(training_missclass_rate)
-    validate_missclass.append(validation_missclass_rate)
+    print(y_train.shape)
 
-#need to plot things now
-plt.figure()
-plt.plot(range(1,13), training_missclass, marker='o', label='Training error')
-plt.plot(range(1,13), validate_missclass, marker='o', label='Validation error')
-plt.title("Training and Test Misclassification Rates, Gisette Dataset")
-plt.xlabel("Maximum depth")
-plt.ylabel("Misclassification error")
-plt.legend()
-plt.grid(True, alpha=0.3)
-#plt.show()
+    #initialize empty arrays
+    train_err = []
+    valid_err = []
 
-#save this image into the figures folder
-plt.savefig("assignments/hw01/figures/gisette_misclassification_p1.png", dpi=400, bbox_inches="tight")
-plt.close()
+    #inner loop looping over the tree depths
+    for d in depths:
+        #fit tree
+        dec_tree = tree.DecisionTreeClassifier(max_depth=d)
+        dec_tree = dec_tree.fit(X_train, y_train)
 
-#need to create a dataframe for table:
-results_q1 = pd.DataFrame({
-    "Depth": range(1,13),
-    "Training Misclassification": training_missclass,
-    "Validation Misclassification": validate_missclass
-})
+        #make predictions
+        training_predictions = dec_tree.predict(X_train)
+        validation_predictions = dec_tree.predict(X_valid)
 
-#find lowest validation error and tree depth
-lowest_error = results_q1[results_q1['Validation Misclassification'] == results_q1['Validation Misclassification'].min()]
+        #compute error
+        training_error = 1 - accuracy_score(y_train, training_predictions)
+        validation_error = 1 - accuracy_score(y_valid, validation_predictions)
 
-#save for latex.  Discovered you can use the .to_latex command. 
-LATEX_table = lowest_error.to_latex(
+        train_err.append(training_error)
+        valid_err.append(validation_error)
+    
+    #now need to plot this
+    plt.figure()
+    plt.plot(list(depths), train_err, marker="o", label="Training error")
+    plt.plot(list(depths), valid_err, marker="o", label="Validation error")
+    plt.title(f"Training and Test Misclassification Rates, {i['name']} Dataset")
+    plt.xlabel("Maximum depth")
+    plt.ylabel("Misclassification error")
+    plt.legend()
+    plt.grid(True, alpha=0.3)
+    plt.savefig(i["fig_out"], dpi=400, bbox_inches="tight")
+    plt.close()
+
+    #need a table to find the best depth
+    results = pd.DataFrame({
+        "Depth": list(depths),
+        "Training Misclassification": train_err,
+        "Validation Misclassification": valid_err
+    })
+
+    #find lowest validation error and tree depth
+    lowest_error = results[results['Validation Misclassification'] == results['Validation Misclassification'].min()]
+
+    #save as latex table:
+    LATEX_table = lowest_error.to_latex(
     index=False,
     caption="Lowest misclassification error, Gisette Dataset",
     label="tab:tree_misclass_error_p1",
     column_format="ccc"
-)
+    )
 
-print(LATEX_table)
-
-
-
-#problem 2, need to repeat this for satimage, madelon, hill-valley, and covtype data
-train_data = pd.read_csv("assignments/hw01/data/X_sat.dat", delim_whitespace=True, header=None)
-train_labels = pd.read_csv("assignments/hw01/data/Y_sat.dat", header=None)
-
-validate_data = pd.read_csv("assignments/hw01/data/Xtest_sat.dat", delim_whitespace=True, header=None)
-validate_labels = pd.read_csv("assignments/hw01/data/Ytest_sat.dat", header=None)
-
-#need to loop from 1 to 12
-#initialize the empty storage arrays
-training_missclass = []
-validate_missclass = []
-
-#12 depths
-for i in range(1, 13):
-    #fitting the decision treee with max depth of i
-    dtree = tree.DecisionTreeClassifier(max_depth=i)
-    dtree = dtree.fit(train_data,train_labels)
-
-    #predict
-    training_predict = dtree.predict(train_data)
-    validate_predict = dtree.predict(validate_data)
-
-    #get misclassification rate
-    #check accuracy scores.  Returns fraction of correctly classified sampler.  I think that this means that the
-    # #misclassification rate is simply 1- this value
-    training_missclass_rate = 1 - accuracy_score(train_labels, training_predict)
-    validation_missclass_rate = 1 - accuracy_score(validate_labels, validate_predict)
-
-    #append to arrays
-    training_missclass.append(training_missclass_rate)
-    validate_missclass.append(validation_missclass_rate)
-
-#need to plot things now
-plt.figure()
-plt.plot(range(1,13), training_missclass, marker='o', label='Training error')
-plt.plot(range(1,13), validate_missclass, marker='o', label='Validation error')
-plt.title("Training and Test Misclassification Rates, Satimage Dataset")
-plt.xlabel("Maximum depth")
-plt.ylabel("Misclassification error")
-plt.legend()
-plt.grid(True, alpha=0.3)
-#plt.show()
-
-#save this image into the figures folder
-plt.savefig("assignments/hw01/figures/satimage_misclassification_p2.png", dpi=400, bbox_inches="tight")
-plt.close()
-
-#need to create a dataframe for table:
-results_q1 = pd.DataFrame({
-    "Depth": range(1,13),
-    "Training Misclassification": training_missclass,
-    "Validation Misclassification": validate_missclass
-})
-
-#find lowest validation error and tree depth
-lowest_error = results_q1[results_q1['Validation Misclassification'] == results_q1['Validation Misclassification'].min()]
-
-#save for latex.  Discovered you can use the .to_latex command. 
-LATEX_table = lowest_error.to_latex(
-    index=False,
-    caption="Lowest misclassification error: Satimage Dataset",
-    label="tab:tree_misclass_error_p2_satimage",
-    column_format="ccc"
-)
-
-print(LATEX_table)
-
-#madelon data
-train_data = pd.read_csv("assignments/hw01/data/madelon_train.data", delim_whitespace=True, header=None)
-train_labels = pd.read_csv("assignments/hw01/data/madelon_train.labels", header=None)
-
-validate_data = pd.read_csv("assignments/hw01/data/madelon_valid.data", delim_whitespace=True, header=None)
-validate_labels = pd.read_csv("assignments/hw01/data/madelon_valid.labels", header=None)
-
-#need to loop from 1 to 12
-#initialize the empty storage arrays
-training_missclass = []
-validate_missclass = []
-
-#12 depths
-for i in range(1, 13):
-    #fitting the decision treee with max depth of i
-    dtree = tree.DecisionTreeClassifier(max_depth=i)
-    dtree = dtree.fit(train_data,train_labels)
-
-    #predict
-    training_predict = dtree.predict(train_data)
-    validate_predict = dtree.predict(validate_data)
-
-    #get misclassification rate
-    #check accuracy scores.  Returns fraction of correctly classified sampler.  I think that this means that the
-    # #misclassification rate is simply 1- this value
-    training_missclass_rate = 1 - accuracy_score(train_labels, training_predict)
-    validation_missclass_rate = 1 - accuracy_score(validate_labels, validate_predict)
-
-    #append to arrays
-    training_missclass.append(training_missclass_rate)
-    validate_missclass.append(validation_missclass_rate)
-
-#need to plot things now
-plt.figure()
-plt.plot(range(1,13), training_missclass, marker='o', label='Training error')
-plt.plot(range(1,13), validate_missclass, marker='o', label='Validation error')
-plt.title("Training and Test Misclassification Rates, Satimage Dataset")
-plt.xlabel("Maximum depth")
-plt.ylabel("Misclassification error")
-plt.legend()
-plt.grid(True, alpha=0.3)
-#plt.show()
-
-#save this image into the figures folder
-plt.savefig("assignments/hw01/figures/madelon_misclassification_p2.png", dpi=400, bbox_inches="tight")
-plt.close()
-
-#need to create a dataframe for table:
-results_q1 = pd.DataFrame({
-    "Depth": range(1,13),
-    "Training Misclassification": training_missclass,
-    "Validation Misclassification": validate_missclass
-})
-
-#find lowest validation error and tree depth
-lowest_error = results_q1[results_q1['Validation Misclassification'] == results_q1['Validation Misclassification'].min()]
-
-#save for latex.  Discovered you can use the .to_latex command. 
-LATEX_table = lowest_error.to_latex(
-    index=False,
-    caption="Lowest misclassification error: madelon Dataset",
-    label="tab:tree_misclass_error_p2_madelon",
-    column_format="ccc"
-)
-
-print(LATEX_table)
-
-#hill and valley dataset
-train_data = pd.read_csv("assignments/hw01/data/X_hv.dat", delim_whitespace=True, header=None)
-train_labels = pd.read_csv("assignments/hw01/data/Y_hv.dat", header=None)
-
-validate_data = pd.read_csv("assignments/hw01/data/Xtest_hv.dat", delim_whitespace=True, header=None)
-validate_labels = pd.read_csv("assignments/hw01/data/Ytest_hv.dat", header=None)
-
-#need to loop from 1 to 12
-#initialize the empty storage arrays
-training_missclass = []
-validate_missclass = []
-
-#12 depths
-for i in range(1, 13):
-    #fitting the decision treee with max depth of i
-    dtree = tree.DecisionTreeClassifier(max_depth=i)
-    dtree = dtree.fit(train_data,train_labels)
-
-    #predict
-    training_predict = dtree.predict(train_data)
-    validate_predict = dtree.predict(validate_data)
-
-    #get misclassification rate
-    #check accuracy scores.  Returns fraction of correctly classified sampler.  I think that this means that the
-    # #misclassification rate is simply 1- this value
-    training_missclass_rate = 1 - accuracy_score(train_labels, training_predict)
-    validation_missclass_rate = 1 - accuracy_score(validate_labels, validate_predict)
-
-    #append to arrays
-    training_missclass.append(training_missclass_rate)
-    validate_missclass.append(validation_missclass_rate)
-
-#need to plot things now
-plt.figure()
-plt.plot(range(1,13), training_missclass, marker='o', label='Training error')
-plt.plot(range(1,13), validate_missclass, marker='o', label='Validation error')
-plt.title("Training and Test Misclassification Rates, Satimage Dataset")
-plt.xlabel("Maximum depth")
-plt.ylabel("Misclassification error")
-plt.legend()
-plt.grid(True, alpha=0.3)
-#plt.show()
-
-#save this image into the figures folder
-plt.savefig("assignments/hw01/figures/hill_valley_misclassification_p2.png", dpi=400, bbox_inches="tight")
-plt.close()
-
-#need to create a dataframe for table:
-results_q1 = pd.DataFrame({
-    "Depth": range(1,13),
-    "Training Misclassification": training_missclass,
-    "Validation Misclassification": validate_missclass
-})
-
-#find lowest validation error and tree depth
-lowest_error = results_q1[results_q1['Validation Misclassification'] == results_q1['Validation Misclassification'].min()]
-
-#save for latex.  Discovered you can use the .to_latex command. 
-LATEX_table = lowest_error.to_latex(
-    index=False,
-    caption="Lowest misclassification error: hv Dataset",
-    label="tab:tree_misclass_error_p2_hv",
-    column_format="ccc"
-)
-
-print(LATEX_table)
-
-
-#covtype dataset
-#still need to work on this
-train_data = pd.read_csv("assignments/hw01/data/covtype.data", header=None)
-
-covtype_train = train_data.iloc[:11340]
-#covtype_valid = train_data.iloc[:3780]
-covtype_test = train_data.iloc[(11340+3780):]
-
-
-
-#Problem 3.  Random Forrests
-train_data = pd.read_csv("assignments/hw01/data/gisette_train.data", delim_whitespace=True, header=None)
-train_labels = pd.read_csv("assignments/hw01/data/gisette_train.labels", header=None)
-
-validate_data = pd.read_csv("assignments/hw01/data/gisette_valid.data", delim_whitespace=True, header=None)
-validate_labels = pd.read_csv("assignments/hw01/data/gisette_valid.labels", header=None)
-
-#getting k values
-k_values = [3, 10, 30, 100, 300]
-train_errors = []
-test_errors = []
-
-for k in k_values:
-    rand_forest = RandomForestClassifier(n_estimators=k, max_features=71)
-
-    #fitting
-    rand_forest = rand_forest.fit(train_data, train_labels)
-
-    #making predictions
-    
+    #print
+    print(LATEX_table)
