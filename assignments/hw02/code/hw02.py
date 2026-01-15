b4123f4510225fdfff828c681553bbc7293825a6
PALMER SWANSON <pjswanson@fsu.edu>
2026-01-15 13:02:49 -0500

    have the data

--- assignments/hw02/code/hw02.py
+++ assignments/hw02/code/hw02.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import numpy as np
 
+#reading in the csv file
 df = pd.read_csv('assignments/hw02/data/OnlineNewsPopularity/OnlineNewsPopularity.csv')
 
-print(df.head())
-
-columns_to_drop = ['url', 'timedelta']
+#dropping the first two columns by name.  space in timedelta
+columns_to_drop = ['url', ' timedelta']
 df_new = df.drop(columns=columns_to_drop)
+
+print(df_new.head())
+
+#selecting all but the last column as the predictors
+X = df_new.iloc[:,:-1]
+
+#selecting the last column as response
+Y = np.log(df_new.iloc[:,-1])
+
