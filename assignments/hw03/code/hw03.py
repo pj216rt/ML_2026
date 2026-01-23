a6d799ca1b7848a54828438f12580502471a2caa
PALMER SWANSON <pjswanson@fsu.edu>
2026-01-23 14:50:44 -0500

    tex file

--- assignments/hw03/code/hw03.py
+++ assignments/hw03/code/hw03.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import roc_curve
+import matplotlib.pyplot as plt
 
 #need to add Dexter dataset
 datasets = [
@@ -23,6 +25,9 @@ datasets = [
 lamb = 0.001
 iters = 200
 
+#need to define the loss function
+
+#Q1
 #loop over datasets
 for d in datasets:
     print(f"{d['name']}")
@@ -39,12 +44,13 @@ for d in datasets:
     #standardize
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.fit_transform(X_valid)
+    X_test_scaled = scaler.transform(X_valid)
 
     #initialize w and create empty vector to store loss values
     w = np.zeros(p)
     losses = []
     
+    print("here")
     #loop over the iterations
     for t in range(iters):
         break
@@ -63,8 +69,70 @@ for d in filtered_datasets:
 
     y_train = pd.read_csv(d["y_train"], header=None).values.ravel()
     y_valid = pd.read_csv(d["y_valid"], header=None).values.ravel()
+    
+    #get shape
+    N, p = X_train.shape
+    
+    #add column of 1s
+    X_train_tilde = np.column_stack([np.ones(len(X_train)), X_train])
+    X_valid_tilde = np.column_stack([np.ones(len(X_valid)),  X_valid])
 
-N, p = X_train.shape
+    #identity matrix needed for analytical solution
+    I = np.eye(p+1)
 
-#lambda reserved
-lamb = 0.001
+    #matrix math
+    LHS = X_train_tilde.T @ X_train_tilde + (lamb*N*I)
+    RHS = X_train_tilde.T @ y_train
+
+    #analytical minimizer of the penalized squared loss function
+    w_hat = np.linalg.solve(LHS, RHS)
+
+    #predictions make using w^T @ x
+    train_score = X_train_tilde @ w_hat
+    test_score = X_valid_tilde @ w_hat
+
+    #need to get sign function
+    yhat_train = np.sign(train_score)
+    yhat_test  = np.sign(test_score)
+
+    # misclassification error
+    train_misclass = np.mean(yhat_train != y_train)
+    test_misclass  = np.mean(yhat_test != y_valid)
+
+    print(f"Train misclassification error: {train_misclass:.4f}")
+    print(f"Test misclassification error:  {test_misclass:.4f}")
+
+    #get this into a latex table?
+    results_df = pd.DataFrame([{
+        "lambda": lamb,
+        "train_misclass_error": train_misclass,
+        "test_misclass_error": test_misclass,
+    }])
+
+    LATEX_table = results_df.to_latex(
+    index=False,
+    caption="Train and Test Misclassification Errors",
+    label="tab:train_test_missclass_errors_hw03",
+    column_format="c" * results_df.shape[1],
+    escape=False
+    )
+    
+    print(LATEX_table)
+
+    #ROC curves
+    #https://scikit-learn.org/stable/modules/generated/sklearn.metrics.roc_curve.html
+    fpr_train, tpr_train, thresholds_train = roc_curve(y_train, train_score)
+    fpr_test, tpr_test, thresholds_test = roc_curve(y_valid, test_score)
+
+    #need to find a way to plot this and the result from Q1
+    plt.figure()
+    plt.plot(fpr_train, tpr_train, label="Train ROC")
+    plt.plot(fpr_test, tpr_test, label="Test ROC")
+    plt.plot([0, 1], [0, 1], linestyle="--", label="Random Classifier")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title(f"{d['name']}: Ridge (squared loss) ROC")
+    plt.legend()
+    plt.show()
+
+#can we augment w?
