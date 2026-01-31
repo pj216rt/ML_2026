c763a19cb4b7c8c42cf7c052ac674ccb3601a936
PALMER SWANSON <pjswanson@fsu.edu>
2026-01-31 14:55:28 -0500

    homework 4

---
+++ assignments/hw04/code/hw04.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+#Q1 datasets
+q1_datasets = [
+    {
+        "name": "Gisette",
+        "X_train": "assignments/hw04/data/gisette_train.data",
+        "y_train": "assignments/hw04/data/gisette_train.labels",
+        "X_valid": "assignments/hw04/data/gisette_valid.data",
+        "y_valid": "assignments/hw04/data/gisette_valid.labels"
+    },
+    {
+        "name": "Dexter",
+        "X_train": "assignments/hw04/data/dexter_train.csv",
+        "y_train": "assignments/hw04/data/dexter_train.labels",
+        "X_valid": "assignments/hw04/data/dexter_valid.csv",
+        "y_valid": "assignments/hw04/data/dexter_valid.labels"
+    }
+]
+
+#Q1
+for d in q1_datasets:
+    #get name of dataset
+    name = d["name"]
+    print(name)
