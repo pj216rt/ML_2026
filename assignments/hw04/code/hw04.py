9a89f8f882efcc672da419a3ff34bd58a0b4a578
PALMER SWANSON <pjswanson@fsu.edu>
2026-02-03 20:30:53 -0500

    typing everything up

--- assignments/hw04/code/hw04.py
+++ assignments/hw04/code/hw04.py
@@ -13,7 +13,8 @@ q1_datasets = [
         "y_train": "assignments/hw04/data/gisette_train.labels",
         "X_valid": "assignments/hw04/data/gisette_valid.data",
         "y_valid": "assignments/hw04/data/gisette_valid.labels",
-        "missclass_1000_out": "assignments/hw04/figures/gisette_misclass_1000.png"
+        "missclass_1000_out": "assignments/hw04/figures/gisette_misclass_1000.png",
+        "FSA_out": "assignments/hw04/figures/gisette_misclass_1000_FSA.png"
     },
     {
         "name": "Dexter",
@@ -21,7 +22,8 @@ q1_datasets = [
         "y_train": "assignments/hw04/data/dexter_train.labels",
         "X_valid": "assignments/hw04/data/dexter_valid.csv",
         "y_valid": "assignments/hw04/data/dexter_valid.labels",
-        "missclass_1000_out": "assignments/hw04/figures/dexter_misclass_1000.png"
+        "missclass_1000_out": "assignments/hw04/figures/dexter_misclass_1000.png",
+        "FSA_out": "assignments/hw04/figures/dexter_misclass_1000_FSA.png"
     }
 ]
 
@@ -51,11 +53,9 @@ eta = 0.0
 features = [10, 30, 100, 300, 1000, 3000]
 
 #we're going to be using logspace anyways
-lambdas_to_search = np.logspace(-4, 0, 100)
+lambdas_to_search = np.logspace(-5, 0, 100)
 
-"""
 results = []
-
 #Q1
 for d in q1_datasets:
     #get name of dataset
@@ -220,15 +220,15 @@ for d in q1_datasets:
         })
 
 #df for both Gisette and Dexter
-df = pd.DataFrame(results)
-print(df)
+df_q1 = pd.DataFrame(results)
+print(df_q1)
 
 #need to send this table to LATEX
-LATEX_table = df.to_latex(
+LATEX_table = df_q1.to_latex(
     index=False,
     caption=None,
     label=None,
-    column_format="c" * df.shape[1],
+    column_format="c" * df_q1.shape[1],
     escape=False
 )
 
@@ -238,12 +238,12 @@ with open("assignments/hw04/output/misclass_table.tex", "w") as f:
 
 #plot the final train and test misclassification error vs # of selected features
 #using a semilog axis
-for name in df["dataset"].unique():
+for name in df_q1["dataset"].unique():
     #create filename to save plot
     outpath = f"assignments/hw04/figures/{name}_misclass_vs_features.png"
 
     #select the results for a given dataset
-    sub = df[df["dataset"] == name].copy()
+    sub = df_q1[df_q1["dataset"] == name].copy()
     
     #works better sorted
     #sub = sub.sort_values("selected_features")
@@ -260,7 +260,6 @@ for name in df["dataset"].unique():
     plt.savefig(outpath, dpi=400, bbox_inches="tight")
     plt.close()
     #plt.show()
-"""
 
 
 #Q2, using same datasets
@@ -270,6 +269,8 @@ s = 0.001
 mu = 100
 iters = 100
 features = [10, 30, 100, 300, 1000, 3000]
+
+results = []
 
 for d in q2_datasets:
      #get name of dataset
@@ -293,7 +294,7 @@ for d in q2_datasets:
     y_train = np.where(y_train > 0, 1.0, -1.0)
     y_valid = np.where(y_valid > 0, 1.0, -1.0)
 
-    #convert labels to 0/1 for likelihood to work
+    #convert labels to 0/1 for likelihood to work with AUC
     y_train_converted_01 = (y_train == 1).astype(int)
     y_valid_converted_01 = (y_valid == 1).astype(int)
 
@@ -308,6 +309,8 @@ for d in q2_datasets:
 
     #get N, p
     N, p = X_train_tilde.shape
+
+    training_loss_1000 = []
 
     #loop over the number of possible features
     for feat in features:
@@ -327,6 +330,9 @@ for d in q2_datasets:
             log_part = np.log1p(exponent_part)
             loss = np.mean(log_part)
 
+            if feat == 1000:
+                training_loss_1000.append(loss)
+
             #no sparsity penalty
             #compute gradient
             part1 = 1.0 / (1.0 + np.exp(yz))
@@ -344,15 +350,117 @@ for d in q2_datasets:
             #get maximum of 0 or the ratio.  Exclude the intercept
             p_feat = p-1
             max_portion = max(0, ratio)
-            Mi = np.round(feat + (p-feat)*max_portion)
+            Mi = np.round(feat + (p_feat-feat)*max_portion)
             Mi = int(Mi)
 
             #need to keep the top Mi variables, not including intercept
-            #sort.  Keep the largest Mi
+            #sort.  Keep the largest Mi.
+            #beta length p.  beta[0] = intercept.  beta[1:] feature coefficients
+            #Mi = # of features permitted at this iteration
+            #https://numpy.org/doc/stable/reference/generated/numpy.argsort.html
             b = beta[1:]
             abs_b = abs(b)
+
+            #first entries in sort will be the smallest.  Need to set all but the last
+            #Mi to 0 aka we need to set the first p_feat-Mi to 0
             sort = np.argsort(abs_b)
-            
+            b[sort[:-Mi]] = 0.0
+            beta[1:] = b
+            #should end up with something that looks like
+            #beta = [1.0, 0.0, 3.0, 5.0, -3.0, 0.0, 0.0, 0.0]
+
         end = time.time()
         run_time = end-start
-            
+
+        #need to make predictions now for misclass and AUC
+        z_train = X_train_tilde @ beta
+        z_test = X_valid_tilde @ beta
+
+        #get probabilities for 1 and 0
+        p_train_1 = np.exp(-np.logaddexp(0.0, -z_train))
+        p_train_0 = np.exp(-np.logaddexp(0.0, z_train))
+
+        p_test_1 = np.exp(-np.logaddexp(0.0, -z_test))
+        p_test_0 = np.exp(-np.logaddexp(0.0, z_test))
+
+        #compute the odds of train and test
+        odds_train = p_train_1/p_train_0
+        odds_test = p_test_1/p_test_0
+
+        #make predictions.  Predcit 1 if Odds > 1
+        #as.int makes things nicer
+        yhat_train = (odds_train > 1.0).astype(int)
+        yhat_test = (odds_test > 1.0).astype(int)
+
+        #misclassification error
+        train_err = np.mean(yhat_train != y_train_converted_01)
+        test_err = np.mean(yhat_test != y_valid_converted_01)
+
+        #compute AUC
+        test_auc = roc_auc_score(y_valid_converted_01, X_valid_tilde@beta)
+
+        #compute how many variables are non 0 aka selected
+        selected = int(np.sum(beta[1:] != 0))
+        print(selected)
+
+        #append to results
+        results.append({
+            "dataset": name,
+            "target_features": feat,
+            "selected_features": selected,
+            "train_misclass": train_err,
+            "test_misclass": test_err,
+            "test_auc": test_auc,
+            "train_time_sec": run_time
+        })
+    
+    #plot the training loss vs iteration number
+    plt.figure()
+    plt.plot(range(1, len(training_loss_1000) + 1), training_loss_1000, marker="o")
+    plt.xlabel("Iteration")
+    plt.ylabel("Training logistic loss")
+    plt.title(f"{name} FSA Training loss vs iteration (k = 1000)")
+    plt.grid(True, linestyle="--", alpha=0.5)
+    plt.savefig(d["FSA_out"], dpi=400, bbox_inches="tight")
+    plt.close()
+    #plt.show()
+
+#df for both Gisette and Dexter
+df_q2 = pd.DataFrame(results)
+print(df_q2)
+
+#need to send this table to LATEX
+LATEX_table = df_q2.to_latex(
+    index=False,
+    caption=None,
+    label=None,
+    column_format="c" * df_q2.shape[1],
+    escape=False
+)
+
+#save latex table
+with open("assignments/hw04/output/misclass_table_q2.tex", "w") as f:
+    f.write(LATEX_table)
+
+#need to plot the train and test errors vs k
+for name in df_q2["dataset"].unique():
+    outpath = f"assignments/hw04/figures/{name}_misclass_TISP_vs_FSA_.png"
+    #make copy of the df
+    sub1 = df_q1[df_q1["dataset"] == name].copy()
+    sub2 = df_q2[df_q2["dataset"] == name].copy()
+
+    plt.figure()
+    #TISP
+    plt.semilogx(sub2["selected_features"].to_numpy(), sub1["train_misclass"].to_numpy(), marker="o", label="TISP Train")
+    plt.semilogx(sub2["selected_features"].to_numpy(), sub1["test_misclass"].to_numpy(), marker="s", label="TISP Test")
+    #FSA
+    plt.semilogx(sub1["selected_features"].to_numpy(), sub1["train_misclass"].to_numpy(), marker="o", label="FSA Train")
+    plt.semilogx(sub1["selected_features"].to_numpy(), sub1["test_misclass"].to_numpy(), marker="s", label="FSA Test")
+
+    plt.xlabel("Number of selected features (k)")
+    plt.ylabel("Misclassification error")
+    plt.title(f"{name}  Misclassification error (TISP ans FSA) vs. k")
+    plt.legend()
+    plt.grid(True, linestyle="--", alpha=0.5)
+    plt.savefig(outpath, dpi=400, bbox_inches="tight")
+    plt.close()
