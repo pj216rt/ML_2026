6179181598bdef3897f3c2e77db4b322220a782c
PALMER SWANSON <pjswanson@fsu.edu>
2026-02-14 22:14:05 -0500

    adding plots for the last part

--- assignments/hw06/code/hw06.py
+++ assignments/hw06/code/hw06.py
@@ -217,9 +217,18 @@ results_d = pd.DataFrame(results)
 print(results_d.head())
 
 
-#plotting stuff.  16 curves on the same plot
-plt.figure()
-plt.close()  
+#plotting stuff.  16 curves on the same plot, for each d.  So two plots
+for d in depths:
+    temp = results_d[results_d["d"] == d].sort_values("n_estimators")
+
+    #each label is a curve.  Need to work on this loop portion
+    for label in temp:
+
+    plt.xlabel("Number of trees n")
+    plt.ylabel("Test Misclassification Error")
+    plt.title(f"Gradient Boosting: Test Error vs n (max_depth = {d})")
+    plt.grid(True, linestyle="--", alpha=0.4)
+    plt.show()  
 
 #need to find row containing the smallest test error
 smallest_test_error = results_d[results_d['Test Misclass. Error'] == results_d['Test Misclass. Error'].min()]
