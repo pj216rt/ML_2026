d51f0c8679667199b6828b547cd81308123055c8
PALMER SWANSON <pjswanson@fsu.edu>
2026-02-22 19:52:49 -0500

    figured out the Conv1d function

--- assignments/hw07/code/hw07.py
+++ assignments/hw07/code/hw07.py
@@ -116,7 +116,7 @@ for epoch in range(epochs_to_use):
         optimizer.step()
 
         #add compute train accuracy
-        outputs =  model(x_batch)
+        #outputs =  model(x_batch)
         soft_outputs = torch.nn.functional.softmax(outputs, dim=1)
         top_class = soft_outputs.topk(1, dim = 1)[1].squeeze()
 
@@ -164,7 +164,9 @@ plt.title("Train and Test Accuracy vs Epoch\nOne Hidden Layer, 256 neurons, ReLu
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
-plt.show()
+plt.savefig("assignments/hw07/figures/part_a.png", dpi=400, bbox_inches="tight")
+plt.close()
+# plt.show()
 
 
 
@@ -213,7 +215,7 @@ for epoch in range(epochs_to_use):
         optimizer.step()
 
         #add compute train accuracy
-        outputs =  model(x_batch)
+        #outputs =  model(x_batch)
         soft_outputs = torch.nn.functional.softmax(outputs, dim=1)
         top_class = soft_outputs.topk(1, dim = 1)[1].squeeze()
 
@@ -261,12 +263,15 @@ plt.title("Train and Test Accuracy vs Epoch\nTwo Hidden Layers, 64 neurons each,
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
-plt.show()
+plt.savefig("assignments/hw07/figures/part_b.png", dpi=400, bbox_inches="tight")
+plt.close()
+#plt.show()
 
 
 #part c.  one hidden layer, 32 filters of size 15.
 #output length is Size of Data - Size of Kernel  + 1: 256-15+1
-#Conv1d.  Issues with the dimensions.  
+#Conv1d.  Issues with the dimensions.  Conv1d needs the data to be
+#shaped differently
 model = nn.Sequential(
     nn.Conv1d(in_channels=1, out_channels=32, kernel_size=15),
     nn.ReLU(),
@@ -290,13 +295,16 @@ for epoch in range(epochs_to_use):
 
     for x_batch, y_batch in train_load:
 
-        #send data to CUDA device
+        #send data to CUDA device.  #Need (N, C_in, L)
+        #https://docs.pytorch.org/docs/stable/generated/torch.nn.Conv1d.html
         x_batch = x_batch.to(device)
         y_batch = y_batch.to(device)
 
+        x_batch = x_batch.unsqueeze(1)
+
         #set gradients to 0
         optimizer.zero_grad()
-
+        
         #forward pass
         outputs = model(x_batch)
 
@@ -310,7 +318,7 @@ for epoch in range(epochs_to_use):
         optimizer.step()
 
         #add compute train accuracy
-        outputs =  model(x_batch)
+        #outputs =  model(x_batch)
         soft_outputs = torch.nn.functional.softmax(outputs, dim=1)
         top_class = soft_outputs.topk(1, dim = 1)[1].squeeze()
 
@@ -329,6 +337,8 @@ for epoch in range(epochs_to_use):
     with torch.no_grad():
         for x_batch, y_batch in test_load:
             x_batch = x_batch.to(device)
+
+            x_batch = x_batch.unsqueeze(1)
             y_batch = y_batch.to(device)
 
             #want predicted class labels
@@ -358,4 +368,221 @@ plt.title("Train and Test Accuracy vs Epoch\n CNN One Hidden Layer, 32 filters, 
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
+plt.savefig("assignments/hw07/figures/part_c.png", dpi=400, bbox_inches="tight")
+plt.close()
+#plt.show()
+
+#part d
+#adding a max pooling size 2, stride 2.  
+#https://docs.pytorch.org/docs/stable/generated/torch.nn.MaxPool1d.html
+#ned to change the dimension of the nn.Linear component
+#https://docs.pytorch.org/docs/stable/generated/torch.nn.LazyLinear.html
+model = nn.Sequential(
+    nn.Conv1d(in_channels=1, out_channels=32, kernel_size=15),
+    nn.ReLU(),
+    nn.MaxPool1d(kernel_size=2, stride=2),
+    nn.Flatten(),
+    nn.LazyLinear(10)
+).to(device)
+
+optimizer = optim.SGD(model.parameters(), momentum=0.9, lr = 0.01)
+training_values = []
+test_values = []
+
+#iterating over epochs.
+#https://codesignal.com/learn/courses/building-a-neural-network-in-pytorch/lessons/training-a-neural-network-model-with-pytorch
+#I think this dual loop works? https://discuss.pytorch.org/t/iterating-through-a-dataloader-object/25437
+for epoch in range(epochs_to_use):
+    #Training
+    #train model once per epoch
+    model.train()
+    train_correct = 0
+    train_total = 0
+
+    for x_batch, y_batch in train_load:
+
+        #send data to CUDA device.  #Need (N, C_in, L)
+        #https://docs.pytorch.org/docs/stable/generated/torch.nn.Conv1d.html
+        x_batch = x_batch.to(device)
+        y_batch = y_batch.to(device)
+
+        x_batch = x_batch.unsqueeze(1)
+
+        #set gradients to 0
+        optimizer.zero_grad()
+        
+        #forward pass
+        outputs = model(x_batch)
+
+        #compute the loss
+        loss = criterion(outputs, y_batch)
+
+        #backward pass
+        loss.backward()
+
+        #optimize the model parameters
+        optimizer.step()
+
+        #add compute train accuracy
+        #outputs =  model(x_batch)
+        soft_outputs = torch.nn.functional.softmax(outputs, dim=1)
+        top_class = soft_outputs.topk(1, dim = 1)[1].squeeze()
+
+        train_correct = train_correct + (top_class == y_batch).sum().item()
+        train_total = train_total + y_batch.size(0)
+
+    train_acc = train_correct / train_total
+
+    #Testing now
+    #predictions
+    # #https://discuss.pytorch.org/t/obtain-probabilities-from-cross-entropy-loss/157259
+    model.eval()
+    test_correct = 0
+    test_total = 0
+
+    with torch.no_grad():
+        for x_batch, y_batch in test_load:
+            x_batch = x_batch.to(device)
+
+            x_batch = x_batch.unsqueeze(1)
+            y_batch = y_batch.to(device)
+
+            #want predicted class labels
+            outputs =  model(x_batch)
+            soft_outputs = torch.nn.functional.softmax(outputs, dim=1)
+            top_class = soft_outputs.topk(1, dim = 1)[1].squeeze()
+
+            test_correct = test_correct + (top_class == y_batch).sum().item()
+            test_total = test_total + y_batch.size(0)
+
+    test_acc = test_correct / test_total
+
+    #append values to empty lists
+    training_values.append(train_acc)
+    test_values.append(test_acc)
+
+    if (epoch + 1) % 10 == 0:
+        print(f"Epoch {epoch+1:3d} | Train acc: {train_acc:.4f} | Test acc: {test_acc:.4f}") 
+
+#plotting this now
+plt.figure()
+plt.plot(range(epochs_to_use), training_values, label="Train Accuracy")
+plt.plot(range(epochs_to_use), test_values, label="Test Accuracy")
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+plt.title("Train and Test Accuracy vs Epoch\n CNN One Hidden Layer, 32 filters, Size 15, ReLu Activation, Max Pool=2, Stride 2")
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.savefig("assignments/hw07/figures/part_d.png", dpi=400, bbox_inches="tight")
+plt.close()
+#plt.show()
+
+#part e
+#max pooling over entire spatial output 1*32 output from the 32 filters 
+#after max pooling
+#https://docs.pytorch.org/docs/stable/generated/torch.nn.MaxPool1d.html
+model = nn.Sequential(
+    nn.Conv1d(in_channels=1, out_channels=32, kernel_size=15),
+    nn.ReLU(),
+    nn.MaxPool1d(kernel_size=242, stride=242),
+    nn.Flatten(),
+    nn.Linear(32, 10)
+).to(device)
+
+optimizer = optim.SGD(model.parameters(), momentum=0.9, lr = 0.01)
+training_values = []
+test_values = []
+
+#iterating over epochs.
+#https://codesignal.com/learn/courses/building-a-neural-network-in-pytorch/lessons/training-a-neural-network-model-with-pytorch
+#I think this dual loop works? https://discuss.pytorch.org/t/iterating-through-a-dataloader-object/25437
+for epoch in range(epochs_to_use):
+    #Training
+    #train model once per epoch
+    model.train()
+    train_correct = 0
+    train_total = 0
+
+    for x_batch, y_batch in train_load:
+
+        #send data to CUDA device.  #Need (N, C_in, L)
+        #https://docs.pytorch.org/docs/stable/generated/torch.nn.Conv1d.html
+        x_batch = x_batch.to(device)
+        y_batch = y_batch.to(device)
+
+        x_batch = x_batch.unsqueeze(1)
+
+        #set gradients to 0
+        optimizer.zero_grad()
+        
+        #forward pass
+        outputs = model(x_batch)
+
+        #compute the loss
+        loss = criterion(outputs, y_batch)
+
+        #backward pass
+        loss.backward()
+
+        #optimize the model parameters
+        optimizer.step()
+
+        #add compute train accuracy
+        #outputs =  model(x_batch)
+        soft_outputs = torch.nn.functional.softmax(outputs, dim=1)
+        top_class = soft_outputs.topk(1, dim = 1)[1].squeeze()
+
+        train_correct = train_correct + (top_class == y_batch).sum().item()
+        train_total = train_total + y_batch.size(0)
+
+    train_acc = train_correct / train_total
+
+    #Testing now
+    #predictions
+    # #https://discuss.pytorch.org/t/obtain-probabilities-from-cross-entropy-loss/157259
+    model.eval()
+    test_correct = 0
+    test_total = 0
+
+    with torch.no_grad():
+        for x_batch, y_batch in test_load:
+            x_batch = x_batch.to(device)
+
+            x_batch = x_batch.unsqueeze(1)
+            y_batch = y_batch.to(device)
+
+            #want predicted class labels
+            outputs =  model(x_batch)
+            soft_outputs = torch.nn.functional.softmax(outputs, dim=1)
+            top_class = soft_outputs.topk(1, dim = 1)[1].squeeze()
+
+            test_correct = test_correct + (top_class == y_batch).sum().item()
+            test_total = test_total + y_batch.size(0)
+
+    test_acc = test_correct / test_total
+
+    #append values to empty lists
+    training_values.append(train_acc)
+    test_values.append(test_acc)
+
+    if (epoch + 1) % 10 == 0:
+        print(f"Epoch {epoch+1:3d} | Train acc: {train_acc:.4f} | Test acc: {test_acc:.4f}") 
+
+#plotting this now
+plt.figure()
+plt.plot(range(epochs_to_use), training_values, label="Train Accuracy")
+plt.plot(range(epochs_to_use), test_values, label="Test Accuracy")
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+plt.title("Train and Test Accuracy vs Epoch\n CNN One Hidden Layer, 32 filters, Size 15, ReLu Activation, Max Pooling")
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.savefig("assignments/hw07/figures/part_e.png", dpi=400, bbox_inches="tight")
+plt.close()
 plt.show()
+
+#part f.  two hidden layers.  each with 16 filters of size 15 with ReLU, max pooling size 2, stride 2
+model = nn.Sequential(
+)
