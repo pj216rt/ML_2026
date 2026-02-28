1f223b85f7ac1daa03ae9a131dab9d1500b63326
PALMER SWANSON <pjswanson@fsu.edu>
2026-02-28 11:38:54 -0500

    figuring out the plotting

--- assignments/hw08/code/hw08.py
+++ assignments/hw08/code/hw08.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
 from sklearn.cluster import KMeans
 from sklearn.metrics.cluster import contingency_matrix
@@ -53,22 +54,73 @@ def make_dataset_part_a(a):
 #https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
 #https://python-course.eu/machine-learning/expectation-maximization-and-gaussian-mixture-models-gmm.php
 #https://scikit-learn.org/stable/modules/generated/sklearn.mixture.GaussianMixture.html#sklearn.mixture.GaussianMixture
+
+#need a way to store the accuracy of each run for both K-means and EM
+acc_kmeans_list  = []
+acc_em_list  = []
+
+rand_kmeans_list = []
+rand_em_list = []
+
+#loop over the different a values
 for a in range(5):
-    #get dataset
-    X, y = make_dataset_part_a(a=a)
+    for run in range(10):
+        #get dataset
+        X, y = make_dataset_part_a(a=a)
 
-    #K-means clustering.  Adding a random seed this time.
-    kmeans = KMeans(n_clusters=2, n_init=10, random_state=1000+a)
-    kmeans_labels = kmeans.fit_predict(X)
+        #K-means clustering.  Adding a random seed this time.
+        kmeans = KMeans(n_clusters=2, n_init=10, random_state=1000+a+run)
+        kmeans_labels = kmeans.fit_predict(X)
 
-    #EM clustering
-    EM_cluster = GaussianMixture(n_components=2, n_init=10, random_state=1000+a)
-    EM_cluster_labels = EM_cluster.fit_predict(X)
+        #EM clustering
+        EM_cluster = GaussianMixture(n_components=2, n_init=10, random_state=1000+a+run)
+        EM_cluster_labels = EM_cluster.fit_predict(X)
 
-    #computing accuracy
-    accuracy_kmeans = conting_matrix(y, kmeans_labels)
-    accuracy_EM = conting_matrix(y, EM_cluster_labels)
+        #computing accuracy
+        accuracy_kmeans = conting_matrix(y, kmeans_labels)
+        accuracy_EM = conting_matrix(y, EM_cluster_labels)
 
-    #the adjusted rand inex
-    rand_index_kmeans = applying_rand_index(y, kmeans_labels)
-    rand_index_EM = applying_rand_index(y, EM_cluster_labels)
+        #append to lists
+        acc_kmeans_list.append(accuracy_kmeans)
+        acc_em_list.append(accuracy_EM)
+
+        #the adjusted rand inex
+        rand_index_kmeans = applying_rand_index(y, kmeans_labels)
+        rand_index_EM = applying_rand_index(y, EM_cluster_labels)
+
+    #plot the clustering results if a==0
+    #https://matplotlib.org/stable/gallery/subplots_axes_and_figures/subplots_demo.html
+    if a == 0 and run == 9:
+        #set up figure
+        fig, axes = plt.subplots(1, 2)
+
+        #K-means plot
+        axes[0].scatter(
+            X[:, 0], X[:, 1],
+            c=kmeans_labels,
+            s=10,
+            alpha=0.7
+            )
+        axes[0].set_title("K-means Clustering (a = 0)")
+        axes[0].set_xlabel("X1")
+        axes[0].set_ylabel("X2")
+
+        #EM plot
+        axes[1].scatter(
+            X[:, 0], X[:, 1],
+            c=EM_cluster_labels,
+            s=10,
+            alpha=0.7
+            )
+        axes[1].set_title("EM Clustering (a = 0)")
+        axes[1].set_xlabel("X1")
+        axes[1].set_ylabel("X2")
+
+        plt.tight_layout()
+        plt.show()
+    
+    #plot the accuracy of each run vs as a seperate dots on the same plot
+    #different colors for K-means and EM
+    # plt.figure()
+    # plt.scatter(
+    # )
